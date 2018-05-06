b7fca901fb03df81add1c2730ceb89a514b66065
EC2 Default User <ec2-user@ip-172-31-34-150.eu-west-1.compute.internal>
2018-05-06 15:32:41 +0000

    Add overview template

--- votes/views.py
+++ votes/views.py
@@ -1,9 +1,12 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
+# import datetime
+from django.shortcuts import render
 from .models import Game
-import datetime
 
 def overview(request):
     games = Game.objects.all()
-    output = ', '.join([game.date.strftime("%Y-%m-%d %H:%M") for game in games])
+    context = {'games': games}
     
-    return HttpResponse(output)
+    return render(request, 'votes/overview.html', context)
+    # output = ', '.join([game.date.strftime("%Y-%m-%d %H:%M") for game in games])
+    # return HttpResponse(output)

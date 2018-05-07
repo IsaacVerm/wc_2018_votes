efcb1714c451cdc071a73a3b5c70ccd58d252e12
EC2 Default User <ec2-user@ip-172-31-34-150.eu-west-1.compute.internal>
2018-05-07 18:45:46 +0000

    Refactor votes views

--- votes/views.py
+++ votes/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Game, Prediction
+from .models import Game, Prediction, User
 from django.http import HttpResponseRedirect, HttpResponse # temp for placeholders
 
 def games(request):
@@ -15,17 +15,29 @@ def make_prediction(request, game_id):
     return render(request, 'votes/make_prediction.html', context)
     
 def predictions(request, game_id):
-    predictions = Prediction.objects.filter(game=Game.objects.get(pk=game_id))
-    context = {'predictions': predictions}
+    predictions = Prediction.objects.filter(game__id = game_id)
     
+    print(predictions.count())
+    if predictions.count() == 0:
+        return render(request, 'votes/no_predictions_yet.html')
+    
+    context = {'predictions': predictions}
     return render(request, 'votes/predictions.html', context)
     
 def confirm_prediction(request, game_id):
-    prediction = Prediction(game = Game.objects.get(pk=game_id), user = request.POST.get('user'), goals_home_team = request.POST.get('home_goals'), goals_away_team = request.POST.get('away_goals'))
-    prediction.save()
+    name_submitted = request.POST.get('user')
+    home_goals_submitted = request.POST.get('home_goals')
+    away_goals_submitted = request.POST.get('away_goals')
     
     selected_game = Game.objects.get(pk=game_id)
+    selected_user = User.objects.get(name = name_submitted)
     
-    context = {'prediction': prediction, 'selected_game': selected_game}
+    prediction = Prediction(game = selected_game,
+                            user = selected_user,
+                            predicted_goals_home = home_goals_submitted,
+                            predicted_goals_away = away_goals_submitted)
+    prediction.save()
+    
+    context = {'prediction': prediction}
     return render(request, 'votes/confirm_prediction.html', context)
     
